caad35491a6a7b1ecc89a12c410916831fdcf7e0
Bronz57 <chbronz88@gmail.com>
2021-10-23 08:45:56 +0200

    Aggiornamento fatto in classe

--- Models/Assenza.cs
+++ Models/Assenza.cs
@@ -3,7 +3,9 @@ namespace bronzetti.christian._5h.docentiAssenti.Models
 {
     public class Assenza
     {   
-        public DateTime Ora { get; set; }
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public int Ora { get; set; }
         public string Classe { get; set; }
         public string DocenteAssente { get; set; }
         public string Sostituto1 { get; set; }
@@ -11,5 +13,10 @@ namespace bronzetti.christian._5h.docentiAssenti.Models
         public string Pagamento { get; set; }
         public string Note { get; set; }
         public string Firma { get; set; }
+
+        public override string ToString()
+        {
+            return $"Data: {this.Data}, Docente: {this.DocenteAssente}, Classe: {this.Classe}";
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-
+using bronzetti.christian._5h.docentiAssenti.Models;
+using System.Collections.Generic;
 namespace bronzetti.christian._5h.docentiAssenti
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Assenza> assenze = new List<Assenza>();
+            Assenza a = new();
+            a.Classe = "5H";
+            a.DocenteAssente = "Luca Bianchi";
+            a.Data = DateTime.Now;
+
+           Assenza b = new();
+           b.Classe = "5I";
+           b.DocenteAssente = "Luca Rossi";
+           b.Data = DateTime.Now;
+           assenze.Add(a);
+           assenze.Add(b);
+
+           foreach(Assenza x in assenze)
+                Console.WriteLine(x);
         }
     }
 }
